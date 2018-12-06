a888958a693b79866b9133a3f2b23b0c48aa2330
Tyler Sutton <309jtsutton@gmail.com>
2018-12-06 07:21:39 -0600

    Added and styled links

--- src/contact-section.js
+++ src/contact-section.js
@@ -8,11 +8,10 @@ const Contact = () => (
     <div className="contact-holder">
       <p className="find-text"> How to find me </p>
       <div className="social-container">
-        <span> Instagram </span>
-        <span> Facebook </span>
-        <span> Twitter </span>
-        <span> Posh </span>
-        <span> Blog </span>
+        <span><a href="https://www.instagram.com/rsutton05/" target="_blank"> Instagram </a></span>
+        <span><a href="https://www.facebook.com/ohmyposhreneesutton/" target="_blank"> Facebook</a> </span>
+        <span><a href="https://Www.reneesposhspa.com">Posh</a></span>
+        <span> Blog coming soon! &#x1f609; </span>
       </div>
     </div>
   </div>
